35cd2af65c66ad711c17d7c7b83ad3a270147a7c
benjamin <benjaminaaron.only@gmail.com>
2026-04-29 20:38:57 +0800

    fix: configure clangd query drivers

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -1,10 +1,22 @@
+local cmd = {
+  "clangd",
+  "--background-index",
+  "--clang-tidy",
+  "--header-insertion=iwyu",
+}
+
+local gxx = vim.fn.exepath("g++")
+if gxx ~= "" then
+  table.insert(cmd, "--query-driver=" .. gxx)
+end
+
+local gcc = vim.fn.exepath("gcc")
+if gcc ~= "" then
+  table.insert(cmd, "--query-driver=" .. gcc)
+end
+
 return {
-  cmd = {
-    "clangd",
-    "--background-index",
-    "--clang-tidy",
-    "--header-insertion=iwyu",
-  },
+  cmd = cmd,
   init_options = {
     clangdFileStatus = true,
   },
